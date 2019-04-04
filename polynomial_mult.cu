b96c470f43a6b705e623eee1651f99c4f72445ec
dzagar <10978376+dzagar@users.noreply.github.com>
2019-04-04 14:52:47 -0400

    Initial commit
    - First attempt

---
+++ polynomial_mult.cu
@@ -0,0 +1,97 @@
+#include <cstdio>
+
+using namespace std;
+
+const int MAX_DEGREE = 1024;
+const int MAX_COEFF = 103;
+
+void random_polynomial(int* p,  int n)
+{
+    srand(40);
+    for (int i=0; i<n; i++) {
+        p[i] = rand() % MAX_COEFF;
+    }
+}
+
+__global__ void calculate_products(int *prods, int *x, int *y, size_t n) 
+{
+    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    prods[index] = x[blockIdx.x] * y[threadIdx.x];
+    
+}
+
+
+__global__ void reduce_polynomial(int *prods, int *ans, size_t n)
+{
+    // combine like terms
+    int i, j;
+    if (blockIdx.x <= (2*n-2)/2)
+    {
+        i = blockIdx.x, j = 0;
+    }
+    else
+    {
+        i = n-1, j = (blockIdx.x % n) + 1;
+    }
+    while (i >= 0 && j < n)
+    {
+        ans[blockIdx.x] += prods[i*n + j];
+        i--;
+        j++;
+    }
+}
+
+int main() {
+    const int n = 128;    
+    int *X = NULL;
+    int *Y = NULL;
+    int *P = NULL; // products
+    X = new int[n];
+    Y = new int[n];
+    P = new int[n*n];
+    Poly = new int[2*n-1];
+
+    random_polynomial(X, n);
+    random_polynomial(Y, n);
+
+    for (int i = 0; i < n*n; i++)
+    {
+        P[i] = 0;
+    }
+
+    // Products
+	
+	int *Xd, *Yd, *Pd;
+	cudaMalloc((void **)&Xd, sizeof(int)*n);
+    cudaMalloc((void **)&Yd, sizeof(int)*n);
+    cudaMalloc((void **)&Pd, sizeof(int)*n*n);
+
+	cudaMemcpy(Xd, X, sizeof(int)*n, cudaMemcpyHostToDevice);
+    cudaMemcpy(Yd, Y, sizeof(int)*n, cudaMemcpyHostToDevice);
+    cudaMemcpy(Pd, P, sizeof(int)*n*n, cudaMemcpyHostToDevice);
+
+	calculate_products<<<n, n>>>(Pd, Xd, Yd, n);
+    cudaMemcpy(P, Pd, sizeof(int)*n*n, cudaMemcpyDeviceToHost);
+    
+    // Sums to final polynomial
+
+    int *Polyd;
+    cudaMalloc((void **)&Polyd, sizeof(int)*2*n-1);
+
+    cudaMemcpy(Pd, P, sizeof(int)*n*n, cudaMemcpyHostToDevice);
+    cudaMemcpy(Polyd, Poly, sizeof(int)*2*n-1, cudaMemcpyHostToDevice);
+
+    // START REDUCTION KERNEL HERE AND JUST FOR-LOOP THRU THE BLOCK
+    reduce_polynomial<<<1, 2*n-2>>>(Pd, Polyd, prodsDim, n);
+    cudaMemcpy(Poly, Polyd, sizeof(int)*2*n-1, cudaMemcpyDeviceToHost);
+
+	for (int i = 0; i < 2*n-1; ++i) printf("%2d ", Poly[i]);
+	printf("\n");
+	
+	cudaFree(Ad);
+    cudaFree(Bd);
+    cudaFree(Pd);
+    cudaFree(Polyd);
+	
+	return 0;
+}
---
+++ serial_c_polynomial_mult.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const int MAX_DEGREE = 1024;
+const int MAX_COEFF = 103;
+
+void random_polynomial(int* p,  int n)
+{
+    for (int i=0; i<n; i++) {
+        p[i] = rand() % MAX_COEFF;
+    }
+}
+
+void multiply_polynomials(int *x, int *y, int size, int *ans, int ansSize)
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            ans[i+j] += x[i] * y[j];
+        }
+    }
+}
+
+void print_polynomial(int* p, int n)
+{
+    for (int i=0; i<n; i++) 
+    {
+      printf("%d ", p[i]);
+    }
+    printf("\n");
+}
+
+int main(void) 
+{
+    int *x;
+    int *y;
+    int *ans;
+    int size;
+
+    srand(40);
+    // Generate degree between 1 and 2^10.
+    //size = rand() % MAX_DEGREE + 1;
+    size = 4;
+    printf("Size: %d\n",size);
+
+    // Generate x
+    x = (int *) malloc(size * sizeof(int));
+    random_polynomial(x, size);
+    print_polynomial(x, size);
+
+    // Generate y
+    y = (int *) malloc(size * sizeof(int));
+    random_polynomial(y, size);
+    print_polynomial(y, size);
+
+    // Initialize answer
+    int ansSize = 2 * size - 1;
+    ans = (int *) malloc(sizeof(int) * ansSize);
+    for (int i = 0; i < ansSize; i++) 
+    {
+        ans[i] = 0;
+    }
+
+    // Multiply polynomials into answer
+    multiply_polynomials(x, y, size, ans, ansSize);
+    print_polynomial(ans, ansSize);
+
+    // Free
+    free(ans);
+    free(x);
+    free(y);
+    return 0;
+}
